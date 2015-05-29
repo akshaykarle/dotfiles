27330a3ea0811bee7f10190ac4cc8bad77e5e41a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-05-29 18:17:07 +0000

    Update the symlink find command

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 set -x
 
 symlink_dotfiles() {
-  for src in $(find -d *.symlink)
+  for src in $(find -maxdepth 2 -name '*.symlink')
   do
     dst="$HOME/.$(basename "${src%.*}")"
     ln -s $(pwd)/$src $dst
