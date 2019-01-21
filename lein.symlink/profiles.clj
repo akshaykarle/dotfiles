a44f59a6648d8b07e08fd169dbe3dccd0fe28de1
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2019-01-21 10:04:52 +0000

    Update lein repl

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,2 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.19.0"][lein-ancient "0.6.15"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.19.0"]
+                  [lein-ancient "0.6.15"]]}
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
 
