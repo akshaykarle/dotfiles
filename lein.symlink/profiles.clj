dd1bf2059485859b78f969cc9acfdee84e279ff5
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2019-08-08 15:04:26 +0100

    update spacemacs and update clojure deps

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.21.1"]
+{:user {:signing {:gpg-key  "BCD30C6B06AB054965ABE939D9A9047993B32AD6"}
+        :plugins [[cider/cider-nrepl "0.21.1"]
                   [lein-ancient "0.6.15"]]}}
 
