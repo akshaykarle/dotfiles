e34e1f399ccbbea46f001bcb53bdd9de2589bb9f
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2019-10-24 13:40:22 +0100

    Update nrepl deps for lein

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:signing {:gpg-key  "BCD30C6B06AB054965ABE939D9A9047993B32AD6"}
-        :plugins [[cider/cider-nrepl "0.21.1"]
-                  [lein-ancient "0.6.15"]]}}
+        :plugins [[cider/cider-nrepl "0.22.4"]
+                  [refactor-nrepl "2.4.0"]
+                  [lein-ancient "0.6.15"]]
+        :dependencies [[nrepl "0.6.0"]]}}
 
