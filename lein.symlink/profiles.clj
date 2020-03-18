69ffa2b4b5edd6c06d9cbf152fa752da2d03b62e
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2020-03-18 16:49:59 +0000

    add yagni to lein profile

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:signing {:gpg-key  "BCD30C6B06AB054965ABE939D9A9047993B32AD6"}
         :plugins [[cider/cider-nrepl "0.22.4"]
                   [refactor-nrepl "2.4.0"]
-                  [lein-ancient "0.6.15"]]
+                  [lein-ancient "0.6.15"]
+                  [venantius/yagni "0.1.7"]]
         :dependencies [[nrepl "0.6.0"]]}}
 
