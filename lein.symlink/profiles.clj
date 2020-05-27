e20b2c62ef9ec94f2355f56b6715762a8ce14f37
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2020-05-27 22:54:22 +0100

    update lein profile to fix cider errors in spacemacs

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,7 +1,8 @@
 {:user {:signing {:gpg-key  "BCD30C6B06AB054965ABE939D9A9047993B32AD6"}
         :plugins [[refactor-nrepl "2.5.0"]
-                  [cider/cider-nrepl "0.24.0"]
+                  [cider/cider-nrepl "0.25.0-alpha1"]
                   [lein-ancient "0.6.15"]
                   [venantius/yagni "0.1.7"]]
-        :dependencies [[nrepl "0.7.0"]]}}
+        :dependencies [[nrepl "0.7.0"]]
+        :repl-options {:nrepl-middleware [refactor-nrepl.middleware/wrap-refactor]}}}}
 
