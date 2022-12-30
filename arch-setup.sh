ee14e3231d0274474eb552989e8b8c46fc3914ae
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2022-12-30 12:39:32 +0000

    add more packages to arch

--- arch-setup.sh
+++ arch-setup.sh
@@ -3,7 +3,7 @@
 set -eu
 
 pamac install snapd libpamac-snap-plugin
-sudo pacman -S curl git
+sudo pacman -S curl git jq jenv
 sudo systemctl enable --now snapd.socket
 if [ ! -d /snap ]
 then
@@ -21,7 +21,7 @@ snap refresh
 
 sudo snap set system experimental.parallel-instances=true
 
-for pkg in docker ngrok brave 1password signal-desktop spotify vlc; do
+for pkg in docker ngrok brave 1password signal-desktop spotify vlc skype libreoffice; do
     snap install $pkg
 done
 
